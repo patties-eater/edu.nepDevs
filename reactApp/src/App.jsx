9b72ebe14d0a6cb14731bd6e5b9b32ebead216c2
patties-eater <anjugautam7223@gmail.com>
2025-11-17 08:49:41 +0545

    vite setup , tailwind setup && supabase done

--- reactApp/src/App.jsx
+++ reactApp/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -18,4 +18,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
